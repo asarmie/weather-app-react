680711da9d7bcfe31c34a97dcbe02036f51039d6
asarmie <asarmiento99@gmail.com>
2022-02-27 15:33:05 -0500

    Added link to open source code

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <h1>Weather Search</h1>
       <SearchEngine />
+      <p>
+        <a href="https://github.com/asarmie/weather-app-react">
+          Open-source code
+        </a>
+        , by Andrea Sarmiento
+      </p>
     </div>
   );
 }
